ffb55cd1ab530630fd6c51ea7ac11db8f45ec569
mastercoy <mastercoy@gmail.com>
2021-10-29 15:08:55 -0300

    teste commit

--- lambdaExpressions/Program.cs
+++ lambdaExpressions/Program.cs
@@ -13,6 +13,7 @@ namespace lambdaExpressions
             //test.CustomWhere(x => yea ? x.Id != Guid.Empty || x.Name == me : x.Name.StartsWith("M"));
             //test.CustomWhere(x => x.Name.Contains("Boo"));
             //test.CustomWhere(x => x.Age > 10);
+            //test.CustomWhere(x => x.Age > 10);
             test.CustomWhere(x => x.Name.Contains("Boo") && x.Age > 10 || x.Name == me);
             Console.WriteLine("replacement Query: {0}", test.Query);
         }
